0a1c3a89d246a59e5a293429e39682968cc99df1
Ashley Collis <ashleycollisburgess@gmail.com>
2020-04-27 23:43:53 -0400

    temporarily changed stack navigation to display page working on

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import BottomTabNavigator from "./BottomTabNavigator";
-import LandingScreen from "../screens/LandingScreen";
-import VerificationScreen from "../screens/VerificationScreen";
-import SignUpScreen from "../screens/SignUpScreen";
-import { connect } from "react-redux";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabNavigator from './BottomTabNavigator';
+import LandingScreen from '../screens/LandingScreen';
+import VerificationScreen from '../screens/VerificationScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import ResponseScreen from '../screens/ResponseScreen';
+import { connect } from 'react-redux';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const mapStateToProps = (state) => ({
   loggedIn: state.auth.loggedIn,
@@ -24,9 +25,10 @@ function Navigation({ containerRef, initialNavigationState, loggedIn }) {
           <Stack.Screen name="Root" component={BottomTabNavigator} />
         ) : (
           <>
-            <Stack.Screen name="Landing" component={LandingScreen} />
+            {/* <Stack.Screen name="Landing" component={LandingScreen} />
             <Stack.Screen name="Verification" component={VerificationScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
+            <Stack.Screen name="Response" component={ResponseScreen} />
           </>
         )}
       </Stack.Navigator>
