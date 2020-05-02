df48c37b1349e71b6bf3788ab9db0697dad11269
awesome94 <david.kironde@andela.com>
2020-05-02 18:17:34 +0300

    add calenderView component

--- screens/CalenderView.js
+++ screens/CalenderView.js
@@ -6,6 +6,30 @@ import {
 } from 'react-native';
 import calendarPicker from 'react-native-calendar-picker';
 
-export default CalenderView()=>{
+const CalenderView = ({})=>{    
+    const [selectedStartDate, SetselectedStartDate] = useState(null)
+    onDateChange(date){
+        selectedStartDate: date
+    }
+    return(
+        <View style={styles.container}>
+        <CalendarPicker
+          onDateChange={this.onDateChange}
+        />
+        <View>
+          <Text>SELECTED DATE:{ startDate }</Text>
+        </View>
+      </View>
 
+    );
 }
+
+export default CalenderView;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFFFFF',
+      marginTop: 100,
+    },
+  });
