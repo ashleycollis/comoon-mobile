ded137b3d90f422c8a53aa2fee145a84acfabe7d
Ashley Collis <ashleycollisburgess@gmail.com>
2020-05-15 23:59:13 -0400

    update

--- screens/ResponseScreen.js
+++ screens/ResponseScreen.js
@@ -3,27 +3,40 @@ import { View, Button, StyleSheet, Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { getResponse } from '../api/pollRoutes';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+const moment = require('moment');
 
 function ResponseScreen() {
-  const [response, setResponse] = useState('');
+  const [responses, setResponses] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
   const [currentDate, setDate] = useState(new Date());
   useEffect(() => {
     async function fetchResponseData() {
       data = await getResponse('ZnyhJAgy6cFm5mfMj9GZ');
-      setResponse(data);
+      setResponses(data.responses);
+      setName(data.name);
+      setDescription(data.description);
+      setGroup(data.dates);
     }
     fetchResponseData();
   }, []);
-  console.log('state', response);
+  // let person = response[participants][0];
+  let first = moment.unix(1591088400).format('YYYY-MM-DD');
+  let test = moment.unix(1591088400).format('YYYY-MM-DD HH:mm:ss');
+  console.log('name', name);
+  console.log('responses', responses);
+  let dates = {
+    '2020-06-01': { marked: true },
+    '2020-05-18': { selected: true, marked: true, selectedColor: 'blue' },
+    '2020-05-19': { disabled: true, disableTouchEvent: true },
+  };
+  dates[first] = { marked: true, dotColor: 'red', activeOpacity: 0 };
+
   return (
     <CalendarList
       current={currentDate}
-      markedDates={{
-        '2020-05-16': { selected: true, marked: true, selectedColor: 'blue' },
-        '2020-05-17': { marked: true },
-        '2020-05-18': { marked: true, dotColor: 'red', activeOpacity: 0 },
-        '2020-05-19': { disabled: true, disableTouchEvent: true },
-      }}
+      markedDates={dates}
       // <ScrollView
       //   style={styles.container}
       //   contentContainerStyle={styles.contentContainer}
