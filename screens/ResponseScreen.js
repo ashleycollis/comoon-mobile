10818cf6bf596b94962b14b143f8a1019f332134
Ashley Collis <ashleycollisburgess@gmail.com>
2020-05-22 17:59:24 -0400

    cleaned, but shows up green for date june 5, that all participants selected

--- screens/ResponseScreen.js
+++ screens/ResponseScreen.js
@@ -21,9 +21,8 @@ function ResponseScreen() {
     fetchResponseData();
   }, []);
   useEffect(() => {
-    let counter = {};
+    let counter = {}; //keeps track of number of people who can meet at that date
     for (let [key, value] of Object.entries(responses)) {
-      // console.log('k', key);
       for (let i = 0; i < value.length; i++) {
         let date = moment.unix(value[i].endTime.seconds).format('YYYY-MM-DD');
         console.log(date);
@@ -33,22 +32,8 @@ function ResponseScreen() {
           counter[date] = 1;
         }
       }
-      // time = value[0].endTime.seconds
-      // console.log('val', value[0].endTime.seconds);
-      // let first = moment.unix(1591088400).format('YYYY-MM-DD');
-      // let day = value.filter((element) => {
-      //   return element.startTime.seconds;
-      // });
-      // console.log('day', day);
-      // if (!dates[key]) {
-      //   dates[key] = value;
-      //   console.log('one', dates[key]);
-      // } else {
-      //   dates[key].push(value);
-      // }
     }
     let available = '';
-    console.log('khk', counter);
     for (let key in counter) {
       if (counter[key] == 3) {
         available = key;
@@ -59,13 +44,11 @@ function ResponseScreen() {
       '2020-06-01': { marked: true },
       '2020-05-18': { selected: true, marked: true, selectedColor: 'blue' },
       '2020-05-19': { disabled: true, disableTouchEvent: true },
-    };
+    }; //the marked sample dates object, but we're only concerned with date added recently
     check[available] = { marked: true, dotColor: 'green', activeOpacity: 0 };
     setDates(check);
-    console.log('dsfcjsdfsf', available);
-    console.log('hshs', dates);
   }, []);
-  let test = moment.unix(1591088400).format('YYYY-MM-DD HH:mm:ss');
+  let test = moment.unix(1591088400).format('YYYY-MM-DD HH:mm:ss'); //how to format later when including time
   return (
     <View>
       <Text>Name: {eventName}</Text>
