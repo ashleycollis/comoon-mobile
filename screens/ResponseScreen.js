b3487249af1a1751d65e58505c49678d2daa1fb5
Ashley Collis <ashleycollisburgess@gmail.com>
2020-05-22 14:30:32 -0400

    feature to light up mutual date

--- screens/ResponseScreen.js
+++ screens/ResponseScreen.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 
 function ResponseScreen() {
   const [responses, setResponses] = useState('');
-  const [name, setName] = useState('');
+  const [eventName, setName] = useState('');
   const [description, setDescription] = useState('');
 
   const [currentDate, setDate] = useState(new Date());
@@ -17,38 +17,63 @@ function ResponseScreen() {
       setResponses(data.responses);
       setName(data.name);
       setDescription(data.description);
-      setGroup(data.dates);
     }
     fetchResponseData();
   }, []);
-  // let person = response[participants][0];
-  let first = moment.unix(1591088400).format('YYYY-MM-DD');
+  useEffect(() => {
+    let counter = {};
+    let dates = {
+      '2020-06-01': { marked: true },
+      '2020-05-18': { selected: true, marked: true, selectedColor: 'blue' },
+      '2020-05-19': { disabled: true, disableTouchEvent: true },
+    };
+    for (let [key, value] of Object.entries(responses)) {
+      // console.log('k', key);
+      for (let i = 0; i < value.length; i++) {
+        let date = moment.unix(value[i].endTime.seconds).format('YYYY-MM-DD');
+        console.log(date);
+        if (counter[date]) {
+          counter[date]++;
+        } else {
+          counter[date] = 1;
+        }
+      }
+      // time = value[0].endTime.seconds
+      // console.log('val', value[0].endTime.seconds);
+      // let first = moment.unix(1591088400).format('YYYY-MM-DD');
+      // let day = value.filter((element) => {
+      //   return element.startTime.seconds;
+      // });
+      // console.log('day', day);
+      // if (!dates[key]) {
+      //   dates[key] = value;
+      //   console.log('one', dates[key]);
+      // } else {
+      //   dates[key].push(value);
+      // }
+    }
+    let available = '';
+    for (let key in counter) {
+      if (counter[key] === 3) {
+        available = key;
+      }
+    }
+    console.log('hey', available);
+    dates[available] = { selected: true, marked: true, selectedColor: 'green' },
+  }, []);
   let test = moment.unix(1591088400).format('YYYY-MM-DD HH:mm:ss');
-  console.log('name', name);
-  console.log('responses', responses);
-  let dates = {
-    '2020-06-01': { marked: true },
-    '2020-05-18': { selected: true, marked: true, selectedColor: 'blue' },
-    '2020-05-19': { disabled: true, disableTouchEvent: true },
-  };
-  dates[first] = { marked: true, dotColor: 'red', activeOpacity: 0 };
 
+  // dates[marked] = { marked: true, dotColor: 'green', activeOpacity: 0 };
+  // dates[first] = { marked: true, dotColor: 'red', activeOpacity: 0 };
   return (
-    <CalendarList
-      current={currentDate}
-      markedDates={dates}
-      // <ScrollView
-      //   style={styles.container}
-      //   contentContainerStyle={styles.contentContainer}
-      // >
-      //   <Agenda />
-      //   <Text>Poll Results</Text>
-      //   <Text>Event: {response.name}</Text>
-      //   <Text>Group: {response.groups[0]}</Text>
-      //   <Text>Location: {response.location}</Text>
-      //   <Text>Participant Availability</Text>
-      // </ScrollView>
-    />
+    <View>
+      <Text>Name: {eventName}</Text>
+      <Text>Description: {description}</Text>
+      <CalendarList
+        current={currentDate}
+        markedDates={dates}
+        />
+    </View>
   );
   // }
 }
